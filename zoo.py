49066fa3884371e65e908f8e32806d0652d8799e
eevlogieva <eevlogieva@yahoo.com>
2014-11-05 14:33:14 +0200

    Add changes to zoo class

--- zoo.py
+++ zoo.py
@@ -3,32 +3,38 @@ from random import random
 
 
 class Zoo:
+    MONEY_PER_ANIMAL = 60
+    DAYS_IN_MONTH = 30
+
     def __init__(self, capacity, budget):
         self.animals = []
         self.capacity = capacity
         self.budget = budget
         self.foods = {"meat": 4, "grass": 2}
 
-    def accomodate_new_animal(self, animal1):
+    def accomodate_new_animal(self, animal):
         if self.capacity == len(self.animals):
             raise ValueError("The capacity is full")
-        for animal in self.animals:
-            if animal.species == animal1.species and animal.name == animal1.name:
+        for zoo_animal in self.animals:
+            if zoo_animal.species == animal.species and zoo_animal.name == animal.name:
                 raise ValueError("There is an animal with the same name")
-        self.animals.append(animal1)
+        self.animals.append(animal)
 
     def daily_income(self):
-        return 60 * len(self.animals)
-        self.budget += 30 * 60 * len(self.animals)
+        return self.MONEY_PER_ANIMAL * len(self.animals)
+        self.budget += self.DAYS_IN_MONTH * self.MONEY_PER_ANIMAL * len(self.animals)
 
-    def daily_outcome(self, animal, food, kilos):
-        animal.eat(kilos)
-        if food == "meat":
-            self.budget -= kilos*self.foods["meat"]
-            return kilos*self.foods["meat"]
-        else:
-            self.budget -= kilos*self.foods["grass"]
-            return kilos*self.foods["grass"]
+    def daily_outcome(self):
+        outcome = 0
+        for animal in self.animals:
+            animal.eat()
+            eaten_food = self.weight * self.food_weight_ratio
+            outcome += eaten_food
+            if animal.food_type == "carnivore":
+                self.budget -= eaten_food * self.foods["meat"]
+            else:
+                self.budget -= eaten_food * self.foods["grass"]
+        return outcome
 
     def die(self, animal1):
         if animal1.die():
@@ -50,10 +56,8 @@ class Zoo:
         self.name = name
 
     def reproduce(self, animal1, animal2, name):
-        if animal1.gender == "female":
-            female = animal1
-        if animal2.gender == "female":
-            female = animal2
-        if animal1.gender != animal2.gender and animal1.species == animal2.species and not female.gestation_period:
+        same_gender = animal1.gender == animal2.gender
+        same_species = animal1.species == animal2.species
+        if not same_gender and same_species:
             if animal1.age >= 2 and animal2.age >= 2:
                 self.newborn(animal1, animal2, name)
